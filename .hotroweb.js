e27a054eb8775676ae96205cf763fc45217b541b
trummod <109177024+trummod@users.noreply.github.com>
2022-08-31 06:18:12 +0700

    Update and rename hotroweb.js to .hotroweb.js

---
+++ .hotroweb.js
@@ -0,0 +1,17 @@
+<b:if cond='data:view.isPost'>
+document.getElementById('there').appendChild(
+    document.getElementById('download')
+
+ );
+  document.getElementById('myhero').appendChild(
+    document.getElementById('custom-hero')
+  ); 
+
+  document.getElementById('extra-target').appendChild(
+    document.getElementById('extra-info')
+  ); 
+
+  document.getElementById('syn-target').appendChild(
+    document.getElementById('synopsis')
+  ); 
+</b:if>
